bea2fa1bcede69011062531a982da9711ea502b5
knee-cola <nikola.derezic@gmail.com>
2018-09-27 22:43:54 +0200

    dorađeno formatiranje

--- docs/script/app.js
+++ docs/script/app.js
@@ -1,23 +1,31 @@
-import { PaymentModels, IntentCodes, TextInput, NumericInput } from 'Components';
+import { PaymentModels, IntentCodes, TextInput, NumericInput, TextAreaInput } from 'Components';
 import { Uplatnica } from 'Uplatnica';
 
 ReactDOM.render(
-    <fieldset>
-        <NumericInput label="Iznos:" />
-        <TextInput label="Račun primatelja:" />
-        <TextInput label="Poziv na broj:" />
-        <TextInput label="Opis plaćanja:" />
-        <PaymentModels />
-        <IntentCodes />
-        <h2>Platitelj</h2>
-        <TextInput label="Ime i prezime:" />
-        <TextInput label="Adresa:" />
-        <TextInput label="Poštanski broj i mjesto:" />
-        <h2>Primatelj</h2>
-        <TextInput label="Ime i prezime:" />
-        <TextInput label="Adresa:" />
-        <TextInput label="Poštanski broj i mjesto:" />
+    <form>
+        <fieldset className="fieldset-platitelj">
+            <h2>Platitelj</h2>
+            <TextInput label="Ime i prezime / Naziv:" />
+            <TextInput label="Adresa:" />
+            <TextInput label="Poštanski broj:" />
+            <TextInput label="Grad/mjesto:" />
+        </fieldset>
+        <fieldset className="fieldset-primatelj">
+            <h2>Primatelj</h2>
+            <TextInput label="Ime i prezime / Naziv:" />
+            <TextInput label="Adresa:" />
+            <TextInput label="Poštanski broj:" />
+            <TextInput label="Grad/mjesto:" />
+        </fieldset>
+        <fieldset className="fieldset-brojke">
+            <TextInput label="IBAN / Račun primatelja:" className="fieldset-brojke__racun" />
+            <NumericInput label="Iznos:" className="fieldset-brojke__iznos" />
+            <br/>
+            <PaymentModels className="fieldset-brojke__model" />
+            <TextInput label="Poziv na broj:" className="fieldset-brojke__na-broj" />
+            <IntentCodes clasName="fieldset-brojke__namjena" />
+            <TextAreaInput label="Opis plaćanja:" />
+        </fieldset>
         <Uplatnica />
-
-    </fieldset>
+    </form>
 , document.getElementById('container') );
--- docs/script/components/Components.js
+++ docs/script/components/Components.js
@@ -1,8 +1,9 @@
+
 class PaymentModels extends React.Component {
     render() {
         return(
-        <div>
-            <label>Model plaćanja:</label>
+        <div className={this.props.className}>
+            <label>Model</label>
             <select>{ BarcodePayment.PaymentModels.map(el => <option key={el.model} value={el.model}>{el.model}</option>) }</select>
         </div>
         );
@@ -12,9 +13,9 @@ class PaymentModels extends React.Component {
 class IntentCodes extends React.Component {
     render() {
         return(
-        <div>
+        <div className={this.props.clasName}>
             <label>Šifra namjene:</label>
-            <select>{ BarcodePayment.IntentCodes.map(el => <option key={el.code} value={el.code}>{el.title}</option>) }</select>
+            <select>{ BarcodePayment.IntentCodes.map(el => <option key={el.code} value={el.code}>{el.code+" -  "+el.title}</option>) }</select>
         </div>
         );
     }
@@ -23,18 +24,27 @@ class IntentCodes extends React.Component {
 class TextInput extends React.Component {
     render() {
         return(
-        <div>
+        <div className={this.props.className}>
             <label>{ this.props.label }</label>
             <input type="text" defaultValue={this.props.defaultValue} />
         </div>
         );
     }
 }
-
+class TextAreaInput extends React.Component {
+    render() {
+        return(
+        <div className={this.props.className}>
+            <label>{ this.props.label }</label>
+            <textarea cols="34" rows="4">{this.props.defaultValue}</textarea>
+        </div>
+        );
+    }
+}
 class NumericInput extends React.Component {
     render() {
         return(
-        <div>
+        <div className={this.props.className}>
             <label>{ this.props.label }</label>
             <input type="number" defaultValue={this.props.defaultValue} />
         </div>
@@ -44,4 +54,4 @@ class NumericInput extends React.Component {
 
 
 
-export { PaymentModels, IntentCodes, TextInput, NumericInput }
+export { PaymentModels, IntentCodes, TextInput, NumericInput, TextAreaInput }
