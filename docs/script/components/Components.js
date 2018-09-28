4276d42b643387eda52b59d0dd7c6501b5b81938
Nikola Derežić <nikola.derezic@gmail.com>
2018-09-28 10:40:22 +0200

    uklonjena duplikacija koda u komponentama

--- docs/script/components/Components.js
+++ docs/script/components/Components.js
@@ -1,34 +1,54 @@
 
-class PaymentModels extends React.Component {
+class GenericSelect extends React.Component {
     render() {
         return(
         <div className={this.props.className}>
-            <label>Model</label>
-            <select>{ BarcodePayment.PaymentModels.map(el => <option key={el.model} value={el.model}>{el.model}</option>) }</select>
+            <label>{this.label}</label>
+            <select id={this.props.id} onChange={this.props.onChange} value={this.props.value}><option key="blank-option"></option>{ this.optionFactory() }</select>
         </div>
         );
     }
 }
-
-class IntentCodes extends React.Component {
-    render() {
-        return(
-        <div className={this.props.clasName}>
-            <label>Šifra namjene:</label>
-            <select>{ BarcodePayment.IntentCodes.map(el => <option key={el.code} value={el.code}>{el.code+" -  "+el.title}</option>) }</select>
-        </div>
-        );
+class PaymentModels extends GenericSelect {
+    constructor() {
+        super();
+        this.label = "Model";
+    }
+    optionFactory() {
+        return(BarcodePayment.PaymentModels.map(el => <option key={el.model} value={el.model}>{el.model}</option>));
     }
 }
 
-class TextInput extends React.Component {
+class IntentCodes extends GenericSelect {
+    constructor() {
+        super();
+        this.label = "Šifra namjene";
+    }
+    optionFactory() {
+        return(BarcodePayment.IntentCodes.map(el => <option key={el.code} value={el.code}>{el.code+" -  "+el.title}</option>));
+    }
+}
+
+class GenericInput extends React.Component {
     render() {
         return(
         <div className={this.props.className}>
             <label>{ this.props.label }</label>
-            <input type="text" defaultValue={this.props.defaultValue} />
+            <input id={this.props.id} type={this.type} value={this.props.value} onChange={this.props.onChange} />
         </div>
         );
+    }
+}
+class TextInput extends GenericInput {
+    constructor() {
+        super();
+        this.type = "text";
+    }
+}
+class NumericInput extends GenericInput {
+    constructor() {
+        super();
+        this.type = "number";
     }
 }
 class TextAreaInput extends React.Component {
@@ -36,22 +56,10 @@ class TextAreaInput extends React.Component {
         return(
         <div className={this.props.className}>
             <label>{ this.props.label }</label>
-            <textarea cols="34" rows="4">{this.props.defaultValue}</textarea>
+            <textarea id={this.props.id} cols="34" rows="4" onChange={this.props.onChange} value={this.props.value} />
         </div>
         );
     }
 }
-class NumericInput extends React.Component {
-    render() {
-        return(
-        <div className={this.props.className}>
-            <label>{ this.props.label }</label>
-            <input type="number" defaultValue={this.props.defaultValue} />
-        </div>
-        );
-    }
-}
-
-
 
 export { PaymentModels, IntentCodes, TextInput, NumericInput, TextAreaInput }
